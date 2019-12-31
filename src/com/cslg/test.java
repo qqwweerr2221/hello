e1c7bd81ba4568a6d44da1e436c65ecffda4690e
allenfen <1050592371@qq.com>
2019-12-31 15:41:05 +0800

    first commit

---
+++ src/com/cslg/test.java
@@ -0,0 +1,14 @@
+package com.cslg;
+
+/**
+ * @Author: Feng Fan
+ * @Date: 2019/12/31
+ * @Description:
+ * @Modify By: dell
+ * @Version:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
